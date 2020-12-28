d4c68f4eabbcaea38b8212f75dcf26a1868aec6f
sondhishubham <sondhi.shubhham11@gmail.com>
2020-12-28 16:19:30 +0530

    Fixed bool to int conversion

--- examples/hello_world.c
+++ examples/hello_world.c
@@ -1,32 +1,32 @@
 
 int printf(char const *format, ...);
 
-/*int it(_Bool m, _Bool t){*/
-/*	return m + t;*/
-/*}*/
-
-/*_Bool bt(int k, char c, _Bool mm){*/
-/*	int kk = k + c + mm;*/
-/*	return kk;*/
-/*}*/
-
-/*char ct(int k, char c, _Bool mm){*/
-/*	int kk = k + c + mm;*/
-/*	return kk;*/
-/*}*/
-/*int main()*/
-/*{*/
-/*	int kk 	= it(1,1);*/
-/*	_Bool t1 = 1;*/
-/*	char ll = bt(10, 'a', t1) + 'a';*/
-/*	_Bool las	= ct(10, 'a', 0);*/
-/*	printf("The int is %d \t The char is %c \t The bool is %d\n",kk,ll,las);*/
-/*}	*/
-
-
-int main(){
-	_Bool a = 2;
-	_Bool b = 1;
-	int l = a + b;
-	printf("%d\n", l);
+int it(_Bool m, _Bool t){
+	return m + t;
 }
+
+_Bool bt(int k, char c, _Bool mm){
+	int kk = k + c + mm;
+	return kk;
+}
+
+char ct(int k, char c, _Bool mm){
+	int kk = k + c + mm;
+	return kk;
+}
+int main()
+{
+	int kk 	= it(1,1);
+	_Bool t1 = 1;
+	char ll = bt(10, 'a', t1) + 'a';
+	_Bool las	= ct(10, 'a', 0);
+	printf("The int is %d \t The char is %c \t The bool is %d\n",kk,ll,las);
+}	
+
+
+/*int main(){*/
+/*	_Bool a = 2;*/
+/*	_Bool b = 1;*/
+/*	int l = a + b;*/
+/*	printf("%d\n", l);*/
+/*}*/
